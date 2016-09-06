6736c4191828a1ba8c2d686e38bd21be597b3d07
Greg Wiley <aztec.rex@jammm.com>
2016-09-05 20:41:04 -0700

    slack information is flowing

---
+++ aws-lambda/src/main/java/com/banjocreek/meeting/lambda/SlackMeetingHandler.java
@@ -0,0 +1,35 @@
+package com.banjocreek.meeting.lambda;
+
+import java.util.function.Function;
+
+public final class SlackMeetingHandler {
+
+    private final Function<String, String> process = new Reverse();
+
+    public SlackResponseBean handle(SlackRequestBean request) {
+
+        // @formatter:off
+        String msg = new StringBuilder()
+
+        .append("channelId").append(": ").append(request.getChannel_id()).append("\n")
+        .append("teamDomain").append(": ").append(request.getTeam_domain()).append("\n")
+        .append("teamId").append(": ").append(request.getTeam_id()).append("\n")
+        .append("text").append(": ").append(request.getText()).append("\n")
+        .append("timestamp").append(": ").append(request.getTimestamp()).append("\n")
+        .append("token").append(": ").append(request.getToken()).append("\n")
+        .append("triggerWord").append(": ").append(request.getTrigger_word()).append("\n")
+        .append("command").append(": ").append(request.getCommand()).append("\n")
+        .append("userId").append(": ").append(request.getUser_id()).append("\n")
+        .append("userName").append(": ").append(request.getUser_name()).append("\n")
+        
+        .append("result").append(": ").append(process.apply(request.getText()))
+        
+        .toString();
+        // @formatter:on
+
+        SlackResponseBean response = new SlackResponseBean();
+        response.setText(msg);
+        return response;
+    }
+
+}
---
+++ aws-lambda/src/main/java/com/banjocreek/meeting/lambda/SlackResponseBean.java
@@ -0,0 +1,18 @@
+package com.banjocreek.meeting.lambda;
+
+/**
+ * Magical fruit.
+ */
+public final class SlackResponseBean {
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    private String text;
+
+}
